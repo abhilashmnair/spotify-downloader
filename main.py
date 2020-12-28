e3fcc49a16a46e4333ca2d589328a5455b475b9e
Abhilash M Nair <abhilashmnair20@gmail.com>
2020-12-28 23:17:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ r = requests.post(tokenUrl, headers=headers, data=data)
 token = r.json()['access_token']
 headers = { "Authorization": "Bearer " + token }
 
-userQuery = input('Enter track title and artist / Spotify URL : ')
 
-search_song(q):
+#Some function definitions
+
+def search_song(q):
     if 'open.spotify.com' in q:
         return q.split('/')[4]
     else:
@@ -30,65 +31,72 @@ search_song(q):
         requestUrl = f"https://api.spotify.com/v1/search?q={query}&type=track"
         response = requests.get(url=requestUrl, headers=headers)
         data = response.json()
-        return data['tracks']['items'][0]['id']
+        try:
+            return data['tracks']['items'][0]['id']
+        except:
+            return None
 
+def downloadViaSpotify(headers,trackId):
+    requestUrl = f"https://api.spotify.com/v1/tracks/{trackId}"
+    response = requests.get(url=requestUrl, headers=headers)
+    data = response.json()
+
+    print('Title : ' + get_title(data))
+    print('Artists : ' + get_artists(data))
+    print('Album : ' + get_album_name(data))
+
+    results = YoutubeSearch(f"{get_title(data)}+{get_artists(data)}", max_results=10).to_dict()
+    youtubeSongUrl = 'https://youtube.com/' + str(results[0]['url_suffix'])
+
+    convertedFileName = f'{get_album_artists(data)}-{get_title(data)}'
+    convertedFilePath = join('.',convertedFileName) + '.mp3'
+
+    if exists(convertedFilePath):
+        print('Already exists!')
+        return None
+
+    print('\nDownloading...')
+    
+    yt = YouTube(youtubeSongUrl)
+    downloadedFilePath = yt.streams.get_audio_only().download(filename=convertedFileName,skip_existing=False)
+
+    print('\nDownloaded file location : ' + str(os.getcwd()))
+    return convertedFilePath,downloadedFilePath,data
+
+def saveMP3(downloadedFilePath,convertedFilePath,data):
+    #FFMPEG Conversion
+    command = f'ffmpeg -v quiet -y -i "{downloadedFilePath}" -acodec libmp3lame -abr true -af "apad=pad_dur=2" -vn -sn -dn -b:a 328k "{convertedFilePath}"'
+    os.system(command)
+
+    audioFile = EasyID3(convertedFilePath)
+    audioFile.delete()
+
+    #Saving track info fetched from Spotify
+    audioFile['title'] = get_title(data)
+    audioFile['tracknumber'] = str(get_track_number(data))
+    audioFile['artist'] = get_artists(data)
+    audioFile['album'] = get_album_name(data)
+    audioFile['albumartist'] = get_album_artists(data)
+    audioFile['originaldate'] = str(get_release_year(data))
+    audioFile.save(v2_version=3)
+
+    #Saving AlbumArt
+    audioFile = ID3(convertedFilePath)
+    audioFile['APIC'] = AlbumCover(encoding=3,mime='image/jpeg',type=3,desc='Album Art',data=get_album_art(data))
+    audioFile.save(v2_version=3)
+
+    #remove unwanted YouTube downloads
+    remove(downloadedFilePath)
+
+#Main program
+
+userQuery = input('Enter track title and artist / Spotify URL : ')
 trackId = search_song(userQuery)
-if None in trackId:
+
+if trackId is None:
     print('Song not found in Spotify Database! Searching in YouTube...')
+    #downloadFromYoutube(userQuery)
+else:
+    convertedFilePath,downloadedFilePath,data = downloadViaSpotify(headers,trackId)
 
-requestUrl = f"https://api.spotify.com/v1/tracks/{trackId}"
-response = requests.get(url=requestUrl, headers=headers)
-data = response.json()
-
-print('Title : ' + get_title(data))
-print('Artists : ' + get_artists(data))
-print('Album : ' + get_album_name(data))
-
-albumArt = get_album_art(data)
-trackTitle = get_title(data)
-trackArtists = get_artists(data)
-albumName = get_album_name(data)
-release_date = get_release_year(data)
-trackNumber = get_track_number(data)
-discNumber = get_disc_number(data)
-
-results = YoutubeSearch(f"{trackTitle}+{trackArtists}", max_results=10).to_dict()
-youtubeSongUrl = 'https://youtube.com/' + str(results[0]['url_suffix'])
-
-albumArtists = get_album_artists(data)
-convertedFileName = f'{albumArtists}-{trackTitle}'
-convertedFilePath = join('.',convertedFileName) + '.mp3'
-
-if exists(convertedFilePath):
-    print('Already exists!')
-
-yt = YouTube(youtubeSongUrl)
-downloadedFilePath = yt.streams.get_audio_only().download(filename=convertedFileName,skip_existing=False)
-
-print('\nDownloaded file location : ' + downloadedFilePath)
-
-#FFMPEG converting
-command = f'ffmpeg -v quiet -y -i "{downloadedFilePath}" -abr true -af "apad=pad_dur=2, dynaudnorm, loudnorm=I=-17" -vn -b:a 320k "{convertedFilePath}"'
-result = os.system(command)
-
-while
-
-audioFile = EasyID3(convertedFilePath)
-audioFile.delete()
-
-#Saving track info fetched from Spotify
-audioFile['title'] = trackTitle
-audioFile['tracknumber'] = str(trackNumber)
-audioFile['artist'] = trackArtists
-audioFile['album'] = albumName
-audioFile['albumartist'] = albumArtists
-audioFile['originaldate'] = release_date
-audioFile.save(v2_version=3)
-
-#Saving AlbumArt
-audioFile = ID3(convertedFilePath)
-audioFile['APIC'] = AlbumCover(encoding=3,mime='image/jpeg',type=3,desc='Album Art',data=albumArt)
-audioFile.save(v2_version=3)
-
-#remove unwanted YouTube downloads
-remove(downloadedFilePath)
+saveMP3(downloadedFilePath,convertedFilePath,data)
