571421db26f3b1588ac4b1a85715bd1e74249069
Abhilash M Nair <abhilashmnair20@gmail.com>
2020-12-29 19:36:21 +0530

    Update args.py

--- args.py
+++ args.py
@@ -1,5 +1,5 @@
-clientId = "6923be29233a454f83f3db90b3172606"
-clientSecret = "c0ec28811f0843d9aeea0a890cca3af2"
-geniusToken = 'YwA_5JkoPe5h6T9tKY5-xnpI42-NTiTWjMawYEBky_m3zvpAAfSrt1X8irIHM08L'
+clientId = "CLIENT_ID"
+clientSecret = "CLIENT_SECRET"
+geniusToken = "GENIUS_TOKEN"
 
 bitrate = '320'
