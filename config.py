e1c1e19f2a903019cfbefa417880da70cd946ec4
Abhilash M Nair <abhilashmnair20@gmail.com>
2020-12-29 10:19:57 +0530

    Update

--- config.py
+++
@@ -1,4 +0,0 @@
-clientId = "YOUR_CLIENT_ID"
-clientSecret = "YOUR_CLIENT_SECRET"
-
-bitrate = '320'
--- definitions.py
+++ definitions.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from urllib.request import urlopen
-from config import *
+from args import *
 import base64
 
 def generate_code():
