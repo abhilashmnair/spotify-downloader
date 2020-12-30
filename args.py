f37d10e6be2fd1e4c3b730f8d4430a8d0ecebca3
Abhilash M Nair <abhilashmnair20@gmail.com>
2020-12-30 09:38:42 +0530

    Delete args.py

--- args.py
+++
@@ -1,5 +0,0 @@
-clientId = "6923be29233a454f83f3db90b3172606"
-clientSecret = "c0ec28811f0843d9aeea0a890cca3af2"
-geniusToken = "PomDAz_YAtmh9Zn9X87lXl8k525tj5ioTiSM1GBPgx8yaYKwtDQUu6RNx3C_M8Zj"
-
-bitrate = '320'
